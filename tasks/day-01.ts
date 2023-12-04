eed976f8534e558b37a3f33907e2286de4b411c8
agnes97 <janachaloupkova97@gmail.com>
2023-12-04 21:12:34 +0100

    ✨ Solve day 1 / part 2

--- tasks/day-01.ts
+++ tasks/day-01.ts
@@ -1,11 +1,17 @@
 import path from "node:path";
 
 const example = Bun.file(path.join(import.meta.dir, "../inputs/day-01/example.txt"));
+const example2 = Bun.file(path.join(import.meta.dir, "../inputs/day-01/example-2.txt"));
 const input = Bun.file(path.join(import.meta.dir, "../inputs/day-01/input.txt"));
 
 const lines = (await input.text()).split("\n");
 
-const getDigits = (text: string) => Array.from(text.match(/\d/g)?.values() ?? []);
+const numbers = new Map(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    .map((number, index) => [number, index + 1]));
+
+const pattern = new RegExp(`(?=(${Array.from(numbers.keys()).join("|")}|\\d))`, "g");
+const getDigits = (text: string) => (Array.from(text.matchAll(pattern), (match) => match[1]) ?? [])
+    .map((digit) => numbers.get(digit) ?? Number(digit));
 
 const findFirstAndLast = <T>(text: T[], type: 'first' | 'last') => 
     text.at(type === 'first' ? 0 : -1);
